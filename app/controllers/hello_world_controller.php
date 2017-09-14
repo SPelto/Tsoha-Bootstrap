3f654f25cc4f724668587725f4431c170ae77972
SPelto <samuli.a.peltonen@helsinki.fi>
2017-09-14 19:24:44 +0300

    14.9.2017

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -1,14 +1,31 @@
 <?php
 
-  class HelloWorldController extends BaseController{
+class HelloWorldController extends BaseController {
 
-    public static function index(){
-      // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
-   	  View::make('home.html');
+    public static function index() {
+        // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
+        echo 'Tämä on etusivu';
     }
 
-    public static function sandbox(){
-      // Testaa koodiasi täällä
-      echo 'Hello World!';
+    public static function sandbox() {
+        // Testaa koodiasi täällä
+        View::make('helloworld.html');
     }
-  }
+
+    public static function login() {
+        View::make('login.html');
+    }
+
+    public static function luoTapahtuma() {
+        View::make('luoTapahtuma.html');
+    }
+
+    public static function tapahtuma_selaa() {
+        View::make('tapahtuma_selaa.html');
+    }
+
+    public static function kokeilu() {
+        echo 'Pääsit tänne!';
+    }
+
+}
--- config/routes.php
+++ config/routes.php
@@ -1,9 +1,21 @@
-<?php
+    <?php
 
-  $routes->get('/', function() {
+    $routes->get('/', function() {
     HelloWorldController::index();
-  });
+    });
 
-  $routes->get('/hiekkalaatikko', function() {
+    $routes->get('/hiekkalaatikko', function() {
     HelloWorldController::sandbox();
-  });
+    });
+
+    $routes->get('/login', function() {
+    HelloWorldController::login();
+    });
+
+    $routes->get('/luoTapahtuma', function() {
+    HelloWorldController::luoTapahtuma();
+    });
+
+    $routes->get('/tapahtuma_selaa', function() {
+    HelloWorldController::tapahtuma_selaa();
+    });
